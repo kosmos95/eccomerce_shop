7f2827f975b1d5cb594a8ca4acc9e58bc10c32bc
ko2 <rheorjs789@naver.com>
2023-07-30 14:48:37 +0900

    장바구니 페이지에 절달할 쿼리

--- src/main/java/shop/study/repository/cartRepository/CartItemRepository.java
+++ src/main/java/shop/study/repository/cartRepository/CartItemRepository.java
@@ -1,8 +1,22 @@
 package shop.study.repository.cartRepository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import shop.study.dto.cartDto.CartDetailDto;
 import shop.study.entity.CartItem;
+
+import java.util.List;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     CartItem findByCartIdAndItemId(Long cartId, Long itemId);
+
+    @Query("select new shop.study.dto.cartDto.CartDetailDto(ci.id, i.itemNm, i.price, ci.count, im.imgUrl) " +  //①
+            "from CartItem ci, ItemImg im " +
+            "join ci.item i " +
+            "where ci.cart.id = :cartId " +
+            "and im.item.id = i.id " +
+            "and im.repimgYn = 'Y' " +
+            "order by ci.regTime desc"
+    )
+    List<CartDetailDto> findCartDetailDtoList(Long cartId);
 }
